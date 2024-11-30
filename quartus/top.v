95a31a9a7e7072950338379ed00501b9ca73a44b
Gabriel Baraldi <baraldsbr@gmail.com>
2024-11-30 12:19:56 -0300

    :tada: first commit

---
+++ quartus/top.v
@@ -0,0 +1,12 @@
+module top(input         clk, reset, 
+           output [31:0] WriteData, DataAdr, 
+           output        MemWrite);
+
+  wire [31:0] PC, Instr, ReadData;
+  
+  // instantiate processor and memories
+  riscvpipeline rvpipeline(clk, reset, PC, Instr, MemWrite, DataAdr, 
+                       WriteData, ReadData);
+  imem imem(PC, Instr);
+  dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+endmodule
