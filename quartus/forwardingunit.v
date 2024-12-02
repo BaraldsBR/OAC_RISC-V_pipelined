25e55642d315d68324cbf55fa061fb0d7e1f9fd8
Gabriel Baraldi <baraldsbr@gmail.com>
2024-12-02 08:33:17 -0300

    bug fix fu

--- quartus/forwardingunit.v
+++ quartus/forwardingunit.v
@@ -4,12 +4,12 @@ module forwardingunit (
     output [1:0]  forwardAE, forwardBE
 );
 
-    assign forwardAE = (rs1E === rdM && rs1E !== 5'b0 && RegWriteM) ? 2'b10 :
-                       (rs1E === rdW && rs1E !== 5'b0 && RegWriteW) ? 2'b01 :
+    assign forwardAE = (rs1E === rdM && rs1E !== 5'b0 && RegWriteM) ? 2'b01 :
+                       (rs1E === rdW && rs1E !== 5'b0 && RegWriteW) ? 2'b10 :
                        2'b00;
 
-    assign forwardBE = (rs2E === rdM && rs2E !== 5'b0 && RegWriteM) ? 2'b10 :
-                       (rs2E === rdW && rs2E !== 5'b0 && RegWriteW) ? 2'b01 :
+    assign forwardBE = (rs2E === rdM && rs2E !== 5'b0 && RegWriteM) ? 2'b01 :
+                       (rs2E === rdW && rs2E !== 5'b0 && RegWriteW) ? 2'b10 :
                        2'b00;
 
 endmodule
